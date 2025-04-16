7ece7d4f979089728b175e1c73e50dbb6a657e75
Laxmi Priya Behera <Laxmi Priya Behera@192.168.1.2>
2025-04-16 20:33:08 +0530

    branch SmokeTest

--- src/test/java/SecondSampleClass.java
+++ src/test/java/SecondSampleClass.java
@@ -3,6 +3,7 @@ public class SecondSampleClass {
 
 	public static void main(String[] args) {
      System.out.println("I'm from SecondSampleClass.java, after modification");
+     System.out.println("HI");
 	}
 
 }
